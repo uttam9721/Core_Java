4c3c61aa364b42b34a57342bea05a2d9d0e97f73
uttam9721 <uttammaurya377@gmail.com>
2025-08-09 23:24:58 +0530

    done

--- Practice/Practice.java
+++ Practice/Practice.java
@@ -1,6 +1,30 @@
+// package Practice;
+
+// public class Practice {
+//     public static void main(String[] args) {
+//         int n = 153;
+//         int count =0;
+//         while (n>0) {
+//             n=n/10;
+//            count++;
+//         }
+//         System.out.println(count);
+//     }
+    
+// }
+
 package Practice;
 
 public class Practice {
-    
-    
+
+    public static void main(String[] args) {
+        int num = 10;
+        int a =1;
+        while (a<=num/2) {
+            if (num%a==0) {
+                System.out.println();
+                // System.out.println();
+            }
+        }
+    }
 }
